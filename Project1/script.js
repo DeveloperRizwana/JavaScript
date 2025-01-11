76f1440819f9219d913f30797198c324f554bc23
devRiz <dev.rizwana.perween01@gmail.com>
2025-01-11 20:08:05 +0530

    learned data types and operators in javascript

--- Project1/script.js
+++ Project1/script.js
@@ -1,5 +1,117 @@
-let js = "amazing";
-if (js === "amazing") alert("JavaScript is FUN!!");
+// let js = "amazing";
+// console.log(40 + 10 + 2 - 10);
 
-40 + 10 + 2 - 10;
-console.log(40 + 10 + 2 - 10);
+// console.log("Rizu");
+// console.log(142020);
+
+// let firstName = "Rizwana";
+// console.log(firstName);
+
+// let lastName = "Perween";
+// console.log(lastName);
+
+// let myFirstAim = "SoftwareEngineer";
+// let mySecondAim = "IAS";
+// let myCurrentAim = "SoftwareEngineer";
+// console.log(myCurrentAim);
+// console.log(mySecondAim);
+
+// assignment -> 1
+// const country = "India";
+// console.log(country);
+
+// // country = 'China';
+// // console.log(country);
+
+// let continent = "Asia";
+// console.log(continent);
+let population = 1.4e9;
+console.log(population);
+
+population++;
+console.log(population);
+let halfPopulation = population / 2;
+console.log(halfPopulation);
+
+const finland = 6e6;
+console.log(population > finland);
+
+let averagePopulation = 33e6;
+console.log(population < averagePopulation);
+
+let description = 'India is in Asia, and its 1.4 billion people speak multiple languages';
+console.log(description);
+
+// // assignment -> 2
+// let isIsland = false;
+// let language;
+
+// console.log(typeof isIsland);
+// console.log(typeof population);
+// console.log(typeof country);
+// console.log(typeof language);
+
+// // assignment -> 3
+// language = 'Hindi';
+// console.log(language);
+
+// // data type 
+// let javascriptIsFun = true;
+// console.log(javascriptIsFun);
+
+// // console.log(typeof true);
+// console.log(typeof javascriptIsFun);
+// // console.log(typeof 23);
+// // console.log(typeof 'Rizu');
+
+// javascriptIsFun = 'YES!';
+// console.log(typeof javascriptIsFun);
+
+// let year;
+// console.log(year);
+// console.log(typeof year);
+
+// year = 2003;
+// console.log(year);
+// console.log(typeof year);
+// console.log(typeof null);
+
+// let age = 20;
+// age = 21;
+
+// const birthYear = 2003;
+// // birthYear = 2004;
+// // const job;
+
+// var job = 'Programmer';
+// console.log(job);
+// job = 'Teacher';
+// console.log(job);
+
+// math operators
+const now = 2025;
+const ageofRizwana = now - 2003;
+const ageOfRazia = now - 2006;
+console.log(ageofRizwana, ageOfRazia);
+
+console.log(ageofRizwana*2, ageofRizwana/10, 2 ** 3);
+
+const firstName = 'Rizwana';
+const lastName = 'Perween';
+console.log(firstName + ' ' + lastName);
+
+// assignment operators
+let x = 10 + 10;
+x += 10; // x -> 30
+x *= 10; // x -> 300
+x /= 10; // x -> 30
+x++; // x -> 31
+x--;    
+console.log("The value of X is : " + x);
+
+//comparision operators - <,>,<=,=>
+console.log(ageofRizwana > ageOfRazia); 
+console.log(ageOfRazia >= 18);
+
+const isFullAge = ageOfRazia >= 18;
+console.log(now - 2003 > now - 2006);
