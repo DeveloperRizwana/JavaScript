f9a0c463d953affafb9d950472a95a40ed28db4b
devRiz <dev.rizwana.perween01@gmail.com>
2025-01-30 13:32:14 +0530

    learned

--- part-2/objects.js
+++ part-2/objects.js
@@ -2,7 +2,7 @@ const Rizwana = {
     firstName: 'Rizwana',
     lastName: 'Perween',
     age: 2025 - 2003,
-    friends: ['Jasmine', 'Nasima', 'Nusrat']
+    friends: ['Jasmine', 'Nasima', 'Nusrat', 'Shabina', 'Umrah', 'Nasima']
 };
 console.log(Rizwana);
 console.log(Rizwana.friends);
@@ -21,3 +21,9 @@ if(Rizwana[interestedIn]){
 } else {
     console.log(`Wrong request! Choose between firstName, lastName, age, job, and friends`);
 }
+
+Rizwana.location = 'India';
+Rizwana['twitter'] = '@DPerween39952';
+console.log(Rizwana);
+
+console.log(`${Rizwana.firstName} has ${Rizwana.friends.length} friends, and her best friend is ${Rizwana.friends[2]}`);
