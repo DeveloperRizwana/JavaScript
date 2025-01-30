05d8f8816e20f1fa9fb964ac23ae8d8a61537fc3
devRiz <dev.rizwana.perween01@gmail.com>
2025-01-30 13:02:58 +0530

    learned about object in javascript

---
+++ part-2/objects.js
@@ -0,0 +1,23 @@
+const Rizwana = {
+    firstName: 'Rizwana',
+    lastName: 'Perween',
+    age: 2025 - 2003,
+    friends: ['Jasmine', 'Nasima', 'Nusrat']
+};
+console.log(Rizwana);
+console.log(Rizwana.friends);
+console.log(Rizwana['age']);
+
+const nameKey = 'Name';
+console.log(Rizwana['first' + nameKey]);
+console.log(Rizwana['last' + nameKey]);
+
+// console.log(Rizwana.'first' + nameKey); // we can't do this
+
+const interestedIn = prompt(`What do you want to know about Rizwana? Choose between firstName, lastName, age and friends`);
+
+if(Rizwana[interestedIn]){
+    console.log(Rizwana[interestedIn]);
+} else {
+    console.log(`Wrong request! Choose between firstName, lastName, age, job, and friends`);
+}
