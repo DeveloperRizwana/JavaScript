d6d14166fa992873cc5692f1fa578e774332954a
DeveloperRizwana <dev.rizwana.perween01@gmail.com>
2025-02-16 12:39:39 +0530

    practiced deep and shallow copy in js

--- Behind the scene/objectReference.js
+++ Behind the scene/objectReference.js
@@ -8,13 +8,44 @@ const rabia = {
   age: 28,
 };
 
-const marriedRabia = rabia;
-marriedRabia.lastName = "Rahil";
+function marryPerson(originalPerson, newLastName) {
+  originalPerson.lastName = newLastName;
+  return originalPerson;
+}
 
-console.log("Before:", rabia);
-console.log("After:", marriedRabia);
+const marriedRabia = marryPerson(rabia, 'Rahil');
+
+// const marriedRabia = rabia;
+// marriedRabia.lastName = 'Rahil';
+console.log('Before:', rabia);
+console.log('After:', marriedRabia);
 
 // rabia = {x:23}; // Errro
 
 rabia.age = 30;
 
+const rabia2 = {
+  firstName: 'Rabia',
+  lastName: 'Perween',
+  age: 28,
+  family: ['Aatif', 'Aahish'],
+};
+
+// Shallow copy
+const copyOfRabia2 = { ...rabia2 };
+copyOfRabia2.lastName = 'Rahil';
+
+// console.log(rabia2, copyOfRabia2);
+// copyOfRabia2.family.push('Anam');
+// copyOfRabia2.family.push('Aahil');
+
+// console.log('Before:', rabia2);
+// console.log('After:', copyOfRabia2);
+
+// Deep copy/clone
+const rabiaClone = structuredClone(rabia2);
+rabiaClone.family.push('Anam');
+rabiaClone.family.push('Aahil');
+
+console.log('Before clone:', rabia2);
+console.log('After clone:', rabiaClone);
