32d8f1e8bbc077f77b2f119bc20941fb6e940717
DeveloperRizwana <dev.rizwana.perween01@gmail.com>
2025-02-22 17:50:33 +0530

    learning rest pattern

---
+++ Data-Structure-Operators/restPatter&Parameter.js
@@ -0,0 +1,54 @@
+'use strict';
+
+const restaurant = {
+  name: 'Spice Delight',
+  location: 'Connaught Place, New Delhi, India',
+  categories: ['Vegetarian', 'Non-Vegetarian', 'Street-Food'],
+  starterMenu: ['Samosa', 'Paneer Tikka', 'Chicken Pakora', 'Dahi Puri'],
+  mainMenu: [
+    'Butter Chicken',
+    'Dal Makhani',
+    'Paneer Butter Masala',
+    'Biryani',
+    'Butter Naan',
+  ],
+
+  openingHours: {
+    thu: {
+      open: 12,
+      close: 22,
+    },
+    fri: {
+      open: 11,
+      close: 23,
+    },
+    sat: {
+      open: 0,
+      close: 24,
+    },
+  },
+
+  order: function (starterIdx, mainIdx) {
+    return [this.starterMenu[starterIdx], this.mainMenu[mainIdx]];
+  },
+
+  orderDeleivery: function ({ starterIdx, mainIdx, time, address }) {
+    console.log(
+      `Order received! ${this.starterMenu[starterIdx]} and  ${this.mainMenu[mainIdx]} will be delivered to ${address} at ${time}`
+    );
+  },
+};
+
+// Spread, because on right side of "="
+const arr = [1, 2, 3, ...[4, 5]];
+
+// Rest, because on left side of "="
+const [a, b, ...others] = [1, 2, 3, 4, 5];
+console.log(a, b, others);
+
+const [ButterChicken, , paneer, ...otherFood] = [
+  ...restaurant.mainMenu,
+  ...restaurant.mainMenu,
+];
+
+console.log(ButterChicken, paneer, otherFood);
