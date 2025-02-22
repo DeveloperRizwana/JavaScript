42bd634f50d83704e3ece1f53a107989614b3c15
DeveloperRizwana <dev.rizwana.perween01@gmail.com>
2025-02-22 19:05:01 +0530

    learned about REST pattern

--- Data-Structure-Operators/restPatter&Parameter.js
+++ Data-Structure-Operators/restPatter&Parameter.js
@@ -37,7 +37,14 @@ const restaurant = {
       `Order received! ${this.starterMenu[starterIdx]} and  ${this.mainMenu[mainIdx]} will be delivered to ${address} at ${time}`
     );
   },
+
+  orderPizza: function (mainIngredient, ...otherIngredient) {
+    console.log(mainIngredient);
+    console.log(otherIngredient);
+  },
 };
+
+// 1) Destructuring
 
 // Spread, because on right side of "="
 const arr = [1, 2, 3, ...[4, 5]];
@@ -48,7 +55,29 @@ console.log(a, b, others);
 
 const [ButterChicken, , paneer, ...otherFood] = [
   ...restaurant.mainMenu,
-  ...restaurant.mainMenu,
+  ...restaurant.starterMenu,
 ];
 
 console.log(ButterChicken, paneer, otherFood);
+
+// Objects
+const { sat, ...weekdays } = restaurant.openingHours;
+console.log(sat, weekdays);
+
+// 2) Functions
+const add = function (...numbers) {
+  //   console.log(numbers);
+  let sum = 0;
+  for (let i = 0; i < numbers.length; i++) {
+    sum += numbers[i];
+  }
+  console.log(sum);
+};
+add(2, 3);
+add(2, 2, 1, 3);
+add(2, 1, 4, 5, 6, 6, 7, 3);
+
+const x = [2, 3, 4];
+add(...x);
+
+restaurant.orderPizza('mushrooms', 'oninon', 'olives', 'spinach');
