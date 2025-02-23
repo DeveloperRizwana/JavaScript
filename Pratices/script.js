a08df21a2afa7ca707003d3a8d3bef4a30d6dd28
DeveloperRizwana <dev.rizwana.perween01@gmail.com>
2025-02-23 16:26:52 +0530

    practiced DOM manipualtion

--- Pratices/script.js
+++
@@ -1,9 +0,0 @@
-function changeContent() {
-  const boxes = document.querySelectorAll(".main-container"); // Select all elements with class "main-container"
-
-  boxes.forEach((box) => {
-    box.style.color = "blue"; // Change text color
-    box.style.border = "2px solid blue"; // Add border for better visibility
-    box.style.padding = "10px";
-  });
-}
