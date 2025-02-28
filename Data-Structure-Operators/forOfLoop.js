66e8a1ae5a040cbdcc4ba340245180ffe9d61f5b
DeveloperRizwana <dev.rizwana.perween01@gmail.com>
2025-02-28 09:57:45 +0530

    learned about for of loop in js

---
+++ Data-Structure-Operators/forOfLoop.js
@@ -0,0 +1,58 @@
+'use strict';
+const restaurant = {
+  name: 'Spice Delight',
+  location: 'Connaught Place, New Delhi, India',
+  categories: ['Vegetarian', 'Non-Vegetarian', 'Street-Food'],
+  starterMenu: ['Samosa', 'Paneer Tikka', 'Chicken Pakora', 'Dahi Puri'],
+  mainMenu: [
+    'Butter Chicken',
+    'Dal Makhani',
+    'Paneer Butter Masala',
+    'Biryani',
+    'Butter Naan',
+  ],
+
+  openingHours: {
+    thu: {
+      open: 12,
+      close: 22,
+    },
+    fri: {
+      open: 11,
+      close: 23,
+    },
+    sat: {
+      open: 0,
+      close: 24,
+    },
+  },
+
+  order: function (starterIdx, mainIdx) {
+    return [this.starterMenu[starterIdx], this.mainMenu[mainIdx]];
+  },
+
+  orderDeleivery: function ({ starterIdx, mainIdx, time, address }) {
+    console.log(
+      `Order received! ${this.starterMenu[starterIdx]} and  ${this.mainMenu[mainIdx]} will be delivered to ${address} at ${time}`
+    );
+  },
+
+  orderPizza: function (mainIngredient, ...otherIngredient) {
+    console.log(mainIngredient);
+    console.log(otherIngredient);
+  },
+};
+
+const menu = [...restaurant.mainMenu, ...restaurant.starterMenu];
+
+for (const item of menu) console.log(item);
+
+// for (const item of menu.entries()) {
+//   console.log(`${item[0] + 1}: ${item[1]}`);
+// }
+
+for (const [i, el] of menu.entries()) {
+  console.log(`${i + 1}: ${el}`);
+}
+
+// console.log([...menu.entries()]);
