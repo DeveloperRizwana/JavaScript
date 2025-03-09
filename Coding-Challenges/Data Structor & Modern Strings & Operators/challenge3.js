9392d6edf9f48e5c45cbd802312a2f5957011d87
DeveloperRizwana <dev.rizwana.perween01@gmail.com>
2025-03-09 12:09:10 +0530

    solved prolbem on udemy

--- Coding-Challenges/Data Structor & Modern Strings & Operators/challenge3.js
+++ Coding-Challenges/Data Structor & Modern Strings & Operators/challenge3.js
@@ -1,24 +1,40 @@
 'use strict';
 const gameEvents = new Map([
   [17, '⚽ GOAL'],
-  [36, ' 🔄 Subtitution'],
+  [36, '🔄 Subtitution'],
   [47, '⚽ GOAL'],
-  [61, ' 🔄 Subtitution'],
+  [61, '🔄 Subtitution'],
   [64, '🟨 Yellow Card'],
   [69, '🔴 Red Card'],
-  [70, ' 🔄 Subtitution'],
-  [72, ' 🔄 Subtitution'],
+  [70, '🔄 Subtitution'],
+  [72, '🔄 Subtitution'],
   [76, '⚽ GOAL'],
   [80, '⚽ GOAL'],
   [92, '🟨 Yellow Card'],
 ]);
 
-const events = new Map(Object.keys(gameEvents));
-console.log(new Set(gameEvents));
+// Problem-1
+const events = [...new Set(gameEvents.values())];
 console.log(events);
 
-for (const [key, value] of gameEvents) {
-  if (key === 64) gameEvents.delete('Yellow Card');
-}
-
+// Problem-2
+gameEvents.delete(64);
 console.log(gameEvents);
+
+// Problem-3
+console.log(
+  `An event happened, on average, every ${90 / gameEvents.size} minutes`
+);
+
+// Bonus Problem
+const time = [...gameEvents.keys()].pop();
+console.log(time);
+console.log(
+  `An event happened, on average, every ${time / gameEvents.size} minutes`
+);
+
+// Problem-4
+for (const [min, event] of gameEvents) {
+  const half = min <= gameEvents.set / 2 ? 'FIRST' : 'SECOND';
+  console.log(`[${half} HALF] ${min}: ${event}`);
+}
