376e4f8bcc06475b59cac93f1fd6ac028333dded
DeveloperRizwana <dev.rizwana.perween01@gmail.com>
2025-03-18 10:24:47 +0530

    practiced callback functions

--- Functions/callBackFunctions.js
+++ Functions/callBackFunctions.js
@@ -4,7 +4,6 @@ const oneWord = function (str) {
   return str.replace(/ /g, '').toUpperCase();
 };
 
-
 const upperFirstWord = function (str) {
   const [first, ...others] = str.split(' ');
   return [first.toUpperCase(), ...others].join(' ');
@@ -21,19 +20,61 @@ const transformer = function (str, fn) {
 transformer('JavaScript is FUN!!', upperFirstWord);
 transformer('JavaScript is FUN!!', oneWord);
 
+// Callback functions ->
 // JS uses callback all the times
+// example-1
 const high5 = function () {
   console.log('👋 ');
 };
 document.body.addEventListener('click', high5);
 
+// example-2
 const frnds = function (name) {
   console.log(
     `${name} is one of the friends..They are together since childhood`
   );
 };
 
+// example-3
 const friendsArray = ['Rizwana', 'Nusrat', 'Jasmine', 'Choti'];
 friendsArray.forEach(frnds);
 
-alert(`These 4 girls are best friends: ${friendsArray.join(', ')}`);
+// example-4
+// alert(`These 4 girls are best friends: ${friendsArray.join(', ')}`);
+
+// example-4
+function greet(name, callback) {
+  console.log('Hello!!', name + ' 😊 ');
+  callback();
+}
+function sayGoodBye() {
+  console.log('Goodbye!!');
+}
+
+greet('Rizwana', sayGoodBye);
+
+// example-5
+function fetchData(callback) {
+  console.log(`Fetching Data.....`);
+
+  setTimeout(() => {
+    // this is a inbuilt function of js
+    console.log('Data Fetched!!!');
+    callback();
+  }, 3000);
+}
+
+function processData() {
+  console.log('Processing Data!!');
+}
+
+fetchData(processData);
+
+// example-6
+console.log('start');
+
+setTimeout(() => {
+  console.log('Maggie cooked in 2 * 60 seconds');
+}, 2000);
+
+console.log('End');
