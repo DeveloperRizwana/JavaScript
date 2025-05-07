fc9845f358dcc621dc8f9d22de72c0ff2f3ad8a2
DeveloperRizwana <dev.rizwana.perween01@gmail.com>
2025-05-07 09:19:10 +0530

    done a challenge on udemy

--- Coding-Challenges/Bankist/script.js
+++ Coding-Challenges/Bankist/script.js
@@ -3,8 +3,22 @@
 const dogsJulia = [3, 5, 2, 12, 7];
 const dogsKate = [4, 1, 15, 8, 3];
 
-
 const ceckDogs = function (dogsJulia, dogsKate) {
-dogsJulia.splice(3,4);
+  //   const correctedDogsJulia = dogsJulia.slice();
+  //   correctedDogsJulia.splice(0, 1);
+  //   correctedDogsJulia.splice(-2);
+  dogsJulia.splice(0, 1);
+  dogsJulia.splice(-2);
 
+  const dogs = dogsJulia.concat(dogsKate);
+  console.log(dogs);
+
+  dogs.forEach(function (val, i) {
+    if (val >= 3) {
+      console.log(`Dog number ${i + 1} is an adult, and ${val} years old...`);
+    } else {
+      console.log(`Dog number ${i + 1} is still  a puppy 🐶`);
+    }
+  });
 };
+ceckDogs(dogsJulia, dogsKate);
